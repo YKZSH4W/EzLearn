50e5708e4c58ca72165d17543be3821650e5de92
Ivanna <ivanuskatbm@gmail.com>
2025-11-21 10:00:54 -0700

    Menu hamburguesa en la actividad

--- scripts/activity.js
+++ scripts/activity.js
@@ -1,27 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+////////////////////////////////////////////////////////////////////////////
+///////  Respuestas
+//////////////////////////////////////////////////////////////////////////7/
+
 const optionA = document.getElementById('optionA');
 const optionB = document.getElementById('optionB');
 const optionC = document.getElementById('optionC');
 const optionD = document.getElementById('optionD');
 
-const messageTitle = document.getElementById('messageTitle');
-const messageBody = document.getElementById('messageBody');
+const messageTitleAnswer = document.getElementById('messageTitle');
+const messageBodyAnswer = document.getElementById('messageBody');
 
-function showModalMessage(isCorrect) {
+function showModalMessageAnswers(isCorrect) {
 
     if (isCorrect) {
-        messageTitle.textContent = 'Correcto!';
-        messageBody.innerHTML = '<p>Respuesta correcta. ¡Muy bien hecho!</p>';
+        messageTitleAnswer.textContent = 'Correcto!';
+        messageBodyAnswer.innerHTML = '<p>Respuesta correcta. ¡Muy bien hecho!</p>';
     } else {
-        messageTitle.textContent = 'Ups...';
-        messageBody.innerHTML = '<p>Respuesta incorrecta. Intenta de nuevo.</p>';
+        messageTitleAnswer.textContent = 'Ups...';
+        messageBodyAnswer.innerHTML = '<p>Respuesta incorrecta. Intenta de nuevo.</p>';
     }
 }
 
-optionA.addEventListener('click', () => showModalMessage(false));
-optionB.addEventListener('click', () => showModalMessage(false));
-optionC.addEventListener('click', () => showModalMessage(false));
-optionD.addEventListener('click', () => showModalMessage(true));
+optionA.addEventListener('click', () => showModalMessageAnswers(false));
+optionB.addEventListener('click', () => showModalMessageAnswers(false));
+optionC.addEventListener('click', () => showModalMessageAnswers(false));
+optionD.addEventListener('click', () => showModalMessageAnswers(true));
+
+////////////////////////////////////////////////////////////////////////////
+///////  Menú
+//////////////////////////////////////////////////////////////////////////7/
+
+const btnActividades = document.getElementById('btnActividades');
+const btnCerrarSesion = document.getElementById('btnCerrarSesion');
+
+const messageBodyMenu = document.getElementById('messageBodyMenu');
+const btnAceptar = document.getElementById('btnAceptar');
+
+function showModalMessageMenu(something){
+    if(something === 'btnActividades'){
+        messageBodyMenu.innerHTML = '<p>¿Estás seguro que deseas Regresar? Si deja la actividad sin completar no se guardará su progreso.</p>';
+        btnAceptar.addEventListener("click", function() {
+            window.location.href = "panel-actividades.html";
+        });
+    }else if (something === 'btnCerrarSesion'){
+        messageBodyMenu.innerHTML = '<p>¿Estás seguro que deseas cerrar sesión?</p>';
+        btnAceptar.addEventListener("click", function() {
+            window.location.href = "welcome.html";
+        });
+    }
+}
+
+btnActividades.addEventListener('click', () => showModalMessageMenu('btnActividades'));
+btnCerrarSesion.addEventListener('click', () => showModalMessageMenu('btnCerrarSesion'));
 
 });
